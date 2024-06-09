391a521b10a39d48b66189c0fc351ec083ddaaac
s-damasceno <samueldamasceno.vip@gmail.com>
2024-06-09 02:28:58 -0300

    Estrutura com classes básicas

---
+++ Matricula.java
@@ -0,0 +1,2 @@
+public class Matricula {
+}
--- Professor.java
+++ Professor.java
@@ -1,52 +1,9 @@
 import java.time.LocalDate;
 import java.util.List;
 
-public class Professor {
-    private String nome;
-    private Endereco endereco;
-    private LocalDate dataDeNascimento;
-    private double salario;
+public class Professor extends Funcionario {
     private List<Turma> turmas;
-
-    public Professor(String nome, Endereco endereco, LocalDate dataDeNascimento, double salario, List<Turma> turmas) {
-        this.nome = nome;
-        this.endereco = endereco;
-        this.dataDeNascimento = dataDeNascimento;
-        this.salario = salario;
-        this.turmas = turmas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public LocalDate getDataDeNascimento() {
-        return dataDeNascimento;
-    }
-
-    public void setDataDeNascimento(LocalDate dataDeNascimento) {
-        this.dataDeNascimento = dataDeNascimento;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
+    private Disciplina disciplina;
 
     public List<Turma> getTurmas() {
         return turmas;
@@ -54,5 +11,13 @@ public class Professor {
 
     public void setTurmas(List<Turma> turmas) {
         this.turmas = turmas;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
     }
 }
